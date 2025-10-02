66c4e18098de509e9c8540a566939883f97845a5
royer53 <9rmlv123@gmail.com>
2025-10-02 10:42:47 -0500

    archivo vista de bienvenido

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/saludo', function () {
+    return ('buenos dias');
+});
+
+Route::get('/saludo', function () {
+    return ('buenos dias');
 });
